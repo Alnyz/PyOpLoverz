caa36bf4c210f97794b5b4f358bddf1aa2afe3cd
Alnyz <katro.coplax@gmail.com>
2019-04-20 10:50:54 +0700

    Update __init__.py

--- pyoploverz/__init__.py
+++ pyoploverz/__init__.py
@@ -2,6 +2,8 @@ from .base import Response
 from .style import LastedOp, LastedResponse, SearchOp, SearchResponse,StreamInfo,StreamOp,StreamDownload
 from .util import LastedOpLoverz, SearchOpLoverz, StreamOpLoverz, Request
 from .api import OpzApi
+__author__ = "Dyseo"
+__version__ ="0.0.1"
 __all__ = [
 	"OpzApi",
 	"Response",
